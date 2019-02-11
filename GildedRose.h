e3940500a49b81d73a644bbd823b61875b8b7a53
ckobayashi714 <ckobayashi@csu.fullerton.edu>
2019-02-11 13:03:26 -0800

    More Edits - Done

--- GildedRose.h
+++ GildedRose.h
@@ -1,8 +1,7 @@
 #pragma once
 
-#include <array>
 #include <string>
-//#include <vector>
+#include <vector>
 using namespace std;
 //using std::string;
 
@@ -14,7 +13,6 @@ public:
     int quality;
     Item(string, int, int);
 };
-
 Item::Item(string new_name, int new_sellIn, int new_quality)
 : name(new_name), sellIn(new_sellIn), quality(new_quality) {
 }
@@ -22,7 +20,7 @@ Item::Item(string new_name, int new_sellIn, int new_quality)
 class GildedRose {
 private:
     // Add something to hold at least 10 items.
-    int* Items;
+    vector<Item> Items;
     size_t sz_;
 
 public:
@@ -36,30 +34,29 @@ public:
 
     Item& operator[](size_t);
 };
+
 GildedRose::GildedRose() {
-  Items = new int[10];
   sz_ = 0;
 }
 GildedRose::GildedRose(size_t size) {
-  Items = new int[size];
   sz_ = 0;
 }
 GildedRose::~GildedRose() {
-  delete[] Items;
-  Items = nullptr;
+  // delete[] Items;
+  // Items = nullptr;
 }
 size_t GildedRose::size() {
   return sz_;
 }
 Item& GildedRose::get(size_t index) {
-  return Items[index];
-  // cout?
+  if (!(index >= 0 && index < Items.size()))
+    throw out_of_range("Error: Out of range.");
+  return Items.at(index);
 }
 void GildedRose::add(const Item& item) {
-  Items[sz_] = item;
+  Items.push_back(item);
   sz_++;
 }
 Item& GildedRose::operator[](size_t item) {
-  return Items[item];
-  //same as get(size t)?? help
+  return get(item);
 }

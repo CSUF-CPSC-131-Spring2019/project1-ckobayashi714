d04021b5044f39d78be64bff6d8356fd9cc21ec7
ckobayashi714 <ckobayashi@csu.fullerton.edu>
2019-02-14 13:30:42 -0800

    Added comments

--- GildedRose.h
+++ GildedRose.h
@@ -21,7 +21,8 @@ class GildedRose {
 private:
     // Add something to hold at least 10 items.
     vector<Item> Items;
-    size_t sz_;
+    size_t sz_,
+           capacity_;
 
 public:
     GildedRose();
@@ -34,29 +35,40 @@ public:
 
     Item& operator[](size_t);
 };
-
+// Default constructor
 GildedRose::GildedRose() {
   sz_ = 0;
+  capacity_ = 0;
 }
+// Non-Defualt Constructor
 GildedRose::GildedRose(size_t size) {
   sz_ = 0;
+  capacity_ = 0;
 }
+// Deconstructor - Deallocate memory
 GildedRose::~GildedRose() {
   // delete[] Items;
   // Items = nullptr;
 }
+// Return the size of the current vector.
 size_t GildedRose::size() {
   return sz_;
 }
+// Retrieve the element at the specified index.
+// Utilize stdexcept to handle edge cases.
 Item& GildedRose::get(size_t index) {
   if (!(index >= 0 && index < Items.size()))
     throw out_of_range("Error: Out of range.");
   return Items.at(index);
 }
+// Insert an element at the next available index of the array.
+// Utilize stdexcept to handle edge cases.
 void GildedRose::add(const Item& item) {
   Items.push_back(item);
   sz_++;
 }
+// Retrieve the element at the specified index.
+// Utilize stdexcept to handle edge cases.
 Item& GildedRose::operator[](size_t item) {
   return get(item);
 }
